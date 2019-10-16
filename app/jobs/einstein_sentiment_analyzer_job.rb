68dc5c5ef1117aa83f8d6118d5efde8c202f968d
Eric Chen <echen1@salesforce.com>
2019-10-16 14:26:55 -0700

    Adding sidekiq and redis queue

--- app/jobs/einstein_sentiment_analyzer_job.rb
+++ app/jobs/einstein_sentiment_analyzer_job.rb
@@ -2,6 +2,7 @@ class EinsteinSentimentAnalyzerJob < ActiveJob::Base
   queue_as :default
 
   def perform(comment)
+    puts "performing sentiment analysis...."
     EinsteinSentimentAnalyzer.new(comment).run
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
@@ -5,4 +6,6 @@ Rails.application.routes.draw do
     post :survey_created
     post :sms_received
   end
+
+  mount Sidekiq::Web => '/sidekiq'
 end
